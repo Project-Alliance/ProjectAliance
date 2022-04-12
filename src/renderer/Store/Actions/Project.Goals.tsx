a4cc3cd353d566c692bcb547431be10474beb0f6
Rehan Goraya <72587992+Rehan-05@users.noreply.github.com>
2022-04-12 15:27:35 +0500

    new chnages

--- src/renderer/Store/Actions/Project.Goals.tsx
+++ src/renderer/Store/Actions/Project.Goals.tsx
@@ -1,6 +1,6 @@
-import Api from "renderer/Api/auth.api";
-import {useSelector} from "react-redux"
-import { AUTH } from "Types/User.types";
+import Api from 'renderer/Api/auth.api';
+import { useSelector } from 'react-redux';
+import { AUTH } from 'Types/User.types';
 
 const GetGoals=(company:string,accessToken:string) =>(dispatch:any)=>{
       dispatch({type:"GET_GOALS_INIT"})
@@ -23,10 +23,16 @@ const GetGoals=(company:string,accessToken:string) =>(dispatch:any)=>{
         debugger
         dispatch({type:"GET_GOALS_ERROR",error:error.response?.data})
       }
+    )
+    .catch((error) => {
+      if (error.message == 'Network Error')
+        dispatch({
+          type: 'GET_GOALS_ERROR',
+          error: { message: 'Network Error' },
+        });
+      else debugger;
+      dispatch({ type: 'GET_GOALS_ERROR', error: error.response?.data });
+    });
+};
 
-      )
-}
-
-export {
-  GetGoals
-}
+export { GetGoals };
